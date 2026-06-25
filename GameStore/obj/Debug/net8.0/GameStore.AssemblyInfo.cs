8fa13bf48035ace3c1070de46616e93d594b5298
Mohammed Alhaji <nuttertools541.541@gmail.com>
2026-06-25 10:10:57 +0200

    Merge branch 'git-test'

--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5db027c2691f6655ad1fb8953ae0db2c516a14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b575bb68a57494df5fbbb243fb5327de9b9e9bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
